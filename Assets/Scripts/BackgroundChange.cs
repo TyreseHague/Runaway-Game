5d4093e7609d4e4bf8b31034fe507a12bb9690f9
TyreseHague <tyresehagueemail@gmail.com>
2022-05-02 20:53:39 +0100

    2nd Last Push (hopefully)

--- Assets/Scripts/BackgroundChange.cs
+++ Assets/Scripts/BackgroundChange.cs
@@ -8,20 +8,19 @@ public class BackgroundChange : MonoBehaviour
     [SerializeField] private Animator background;    
     
     
-    [SerializeField] private string backgroundChanging = "backgroundChange";
+    [SerializeField] private string backgroundChanging = "BackgroundColourChange"; //allows for you to change the animation. this means this script could be used in other scenarios
 
 
 
-    // Update is called once per frame
-    void Update()
-    {
         void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("Player") == true) //checks if the object collided with is the player
             {
-                backgroundImage.Play(background, 0, 0.0f);
-            }
+                background.Play(backgroundChanging, 0, 0.0f); //plays the animation with 0 delay
+                Debug.Log("Background should change: ");
+                Destroy(gameObject); //destroys the trigger, so not to trigger the animation again
+        }
 
         }
-    }
+    
 }
